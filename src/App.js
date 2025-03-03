c249b82a9a383507f429dd0ba89040fbdcd6439b
awnetcode <155958835+awnetcode@users.noreply.github.com>
2025-03-03 11:32:45 +0100

    structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import './App.css';
+
+import Home from './assets/pages/Home';
+import Navbar from './components/Navbar';
+import ExerciseDetail from './assets/pages/ExerciseDetail';
+import Footer from './components/Footer';
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px" sx={{width: {xl: '1488px'}}} m="auto">
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/exercise/:id" element={<ExerciseDetail />}/>
+        </Routes>
+        <Footer />
+    </Box>
   )
 }
 
